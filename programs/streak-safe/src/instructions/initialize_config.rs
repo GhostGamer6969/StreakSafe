3b0ed15079cc13f0edfebd65a44d6ac22c0b6ca0
GhostGamer6969 <arambhmani@hotmail.com>
2025-08-01 10:07:31 +0530

    initialize config

--- programs/streak-safe/src/instructions/initialize_config.rs
+++ programs/streak-safe/src/instructions/initialize_config.rs
@@ -1,15 +1,43 @@
 use anchor_lang::prelude::*;
 
+use crate::Config;
+
 #[derive(Accounts)]
 pub struct InitializeConfig<'info> {
     #[account(mut)]
     pub admin: Signer<'info>,
 
+    #[account(
+        init,
+        space = 8 + Config::INIT_SPACE,
+        payer = admin,
+        seeds = [b"config"],
+        bump,
+    )]
+    pub config: Account<'info, Config>,
+
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> InitializeConfig<'info> {
-    pub fn initialize_config(&mut self, bumps: &InitializeConfigBumps) -> Result<()> {
-        todo!()
+    pub fn initialize_config(
+        &mut self,
+        min_stake: u64,
+        expiry_sec: i64,
+        max_checkin_gap_sec: i64,
+        min_checkin_gap_sec: i64,
+        min_votes: u8,
+        bumps: &InitializeConfigBumps,
+    ) -> Result<()> {
+        self.config.set_inner(Config {
+            min_stake,
+            slash_receiver: Pubkey::from_str_const("DWyWmTCLqfLAzfeiaDZmxVa2Y8qWaehYyHsiFtpPNfND"),
+            expiry_sec,
+            max_checkin_gap_sec,
+            min_checkin_gap_sec,
+            min_votes,
+            bump: bumps.config,
+        });
+        Ok(())
     }
 }
--- programs/streak-safe/src/state/config.rs
+++ programs/streak-safe/src/state/config.rs
@@ -1,12 +1,13 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct Config {
-    min_stake: u64,
-    slash_receiver: Pubkey,
-    expiry_sec: i64,
-    max_checkin_gap_sec: i64,
-    min_checkin_gap_sec: i64,
-    min_votes: u8,
-    bump: u8,
+    pub min_stake: u64,
+    pub slash_receiver: Pubkey,
+    pub expiry_sec: i64,
+    pub max_checkin_gap_sec: i64,
+    pub min_checkin_gap_sec: i64,
+    pub min_votes: u8,
+    pub bump: u8,
 }
